0a8d466b7c2957a785c39410ff8db53ee4372866
kh9486 <fbrkdgus1101@gmail.com>
2022-11-06 20:42:05 +0900

    [22.11.06,류강현](Feat) 서버 구축 및 통신 시스템 개발

---
+++ server/constants/Constants.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wilso on 2018-02-03.
+ */
+
+const SERVER_PORT = 8000;
+const REFRESH_RATE = 25;
+
+const X_STARTING_POS = 100;
+const Y_STARTING_POS = 100;
+const PLAYER_SPEED = 10;
+const STARTING_DIR = 'down';
+const MONGO_REPO = "Account";
+
+const RPS = {
+    PAPER: "Paper",
+    SCISSOR: "Scissors",
+    ROCK: "Rock"
+};
