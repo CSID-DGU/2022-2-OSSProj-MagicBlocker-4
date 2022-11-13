b700d4e6dff2cab96bd7e25a47dfc0b7b09235b4
a <a>
2022-11-13 14:43:55 +0900

    [22.11.13,전재호](fix) gulpfile 경로 수정완료

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const gulp_concat = require('gulp-concat');
 
 function job(){
-    return gulp.src(['server/app.js','server/**/*.js'])
+    return gulp.src(['server_data/**/*.js','server_app/**/*.js'])
     .pipe(gulp_concat('completeApp.js'))
     .pipe(gulp.dest('./'));
 
