d4fc54c70019e11aa6f87620e81a96a0594235c9
88sprout <88sprout@gmail.com>
2022-11-15 07:48:20 +0900

    [22.11.15,전재호](feat) 화면 오토스케일링 기능(canvas렌더링) 추가

--- server_data/constants/Constants.js
+++ server_data/constants/Constants.js
@@ -1,15 +1,17 @@
 /**
- * Created by wilso on 2018-02-03.
+ * 서버에서 사용되는 상수 모음
  */
 
 const SERVER_PORT = 8000;
 const REFRESH_RATE = 25;
 
-const X_STARTING_POS = 100;
-const Y_STARTING_POS = 100;
+const X_STARTING_POS = 500;
+const Y_STARTING_POS = 200;
 const PLAYER_SPEED = 10;
 const STARTING_DIR = 'down';
 const MONGO_REPO = "Account";
+
+const PROJECTILE_SPEED = 10;
 
 /*
 const RPS = {
