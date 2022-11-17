c1d7b4d9068c31baa14d86f5c8d728966a31caf3
Seongwook Baek <subaek0704@dongguk.edu>
2022-11-17 18:10:14 +0900

    [22.11.17,백성욱(feat) 윈도우 크기 조절 시 캐릭터 위 글씨 작아지는 문제 해결]

--- completeApp.js
+++ completeApp.js
@@ -1,3 +1,25 @@
+/**
+ * 서버에서 사용되는 상수 모음
+ */
+
+const SERVER_PORT = 8000;
+const REFRESH_RATE = 25;
+
+const X_STARTING_POS = 500;
+const Y_STARTING_POS = 200;
+const PLAYER_SPEED = 10;
+const STARTING_DIR = 'down';
+const MONGO_REPO = "Account";
+
+const PROJECTILE_SPEED = 10;
+
+/*
+const RPS = {
+    PAPER: "Paper",
+    SCISSOR: "Scissors",
+    ROCK: "Rock"
+};
+*/
 
 /**
  * Player 클래스
@@ -84,28 +106,6 @@
         this.y += this.speed;
     };
 }
-/**
- * 서버에서 사용되는 상수 모음
- */
-
-const SERVER_PORT = 8000;
-const REFRESH_RATE = 25;
-
-const X_STARTING_POS = 500;
-const Y_STARTING_POS = 200;
-const PLAYER_SPEED = 10;
-const STARTING_DIR = 'down';
-const MONGO_REPO = "Account";
-
-const PROJECTILE_SPEED = 10;
-
-/*
-const RPS = {
-    PAPER: "Paper",
-    SCISSOR: "Scissors",
-    ROCK: "Rock"
-};
-*/
 
 /**
  * 게임 서버 메인앱
