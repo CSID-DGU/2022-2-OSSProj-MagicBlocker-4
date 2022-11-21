6c7702dc0e87c328a92ff0b47d6dc2703c30a661
Seongwook Baek <subaek0704@dongguk.edu>
2022-11-21 22:17:16 +0900

    [22.11.21,백성욱](feat)창 크기에 맞게 dom 사이즈 조정

---
+++ client/scripts/ui.js
@@ -0,0 +1,76 @@
+//
+// Ui.js
+//
+Ui={
+
+    create_login_ui:function(){
+        const ui_login = document.getElementById("ui_login");
+        const ui_game = document.getElementById("ui_game");
+
+        //동적 ui 생성
+        const ui_title = document.createElement('div');
+        ui_title.classList.add('ui');
+        ui_title.classList.add('title');
+        ui_title.innerHTML='Magic Blocker';
+
+        ui_login.appendChild(ui_title);
+
+        const ui_name_input = document.createElement('input');
+        ui_name_input.classList.add('ui');
+        ui_name_input.id='username_input';
+        ui_name_input.setAttribute('placeholder','Please Enter Nickname');
+
+        ui_login.appendChild(ui_name_input);
+
+        const ui_play_button = document.createElement('button');
+        ui_play_button.classList.add('ui');
+        ui_play_button.id='play_button';
+        ui_play_button.innerHTML='Play';
+        ui_login.appendChild(ui_play_button);
+
+        const ui_charactor_select = document.createElement('div');
+        ui_charactor_select.classList.add('ui');
+        ui_charactor_select.classList.add('charactor-select');
+        ui_charactor_select.innerHTML="여기에 캐릭터 선택창 떠야됨"
+
+        ui_login.appendChild(ui_charactor_select);
+
+        const ui_how_to_play_button = document.createElement('button');
+        ui_how_to_play_button.classList.add('ui');
+        ui_how_to_play_button.classList.add('how-to-play');
+        ui_how_to_play_button.innerHTML='How to Play?';
+
+        ui_login.appendChild(ui_how_to_play_button);
+
+        const ui_guide_page = document.createElement('div');
+        ui_guide_page.classList.add('ui');
+        ui_guide_page.classList.add('guide');
+        ui_guide_page.id = "guideID";
+        ui_guide_page.innerHTML='이동 : w a s d \n 발사 : k';
+
+        const ui_player_list_box = document.createElement('div');
+        ui_player_list_box.id = 'ui_player_list_box';
+        ui_player_list_box.innerHTML='접속중인 플레이어';
+
+        ui_game.appendChild(ui_player_list_box);
+
+        const player_list = document.createElement('div');
+        player_list.id = 'player_list';
+
+        ui_player_list_box.appendChild(player_list);
+
+        
+
+        ui_how_to_play_button.onclick = function(){ // How to Play? 클릭 시 도움말 뜨도록
+            if(document.getElementById("guideID")==null){
+                ui_login.appendChild(ui_guide_page);
+            }else{
+                ui_guide_page.remove();
+            }
+            
+        }
+
+    }
+}
+
+
