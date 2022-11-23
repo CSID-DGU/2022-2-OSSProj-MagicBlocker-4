75dcbfa7446e7f1f8d290f2e2a5bc051ec36c645
kh9486 <fbrkdgus1101@gmail.com>
2022-11-23 11:57:32 +0900

    [22.11.23,류강현](feat) p5 렌더링 및 shallow copy 다중 draw 구현 완료

--- client/scripts/Keyboard.js
+++ client/scripts/Keyboard.js
@@ -8,32 +8,36 @@ Keyboard={
     leftkey:65,//a
     rightkey:68,//r
     attackkey:75,//k
-
-    control_down:function(event){
-        //if (!inTextField(event)) //채팅창에 포커싱이 되어있을때, 방향키 입력이 안먹게 하는 코드
-            if (event.keyCode == rightkey) 
-                mySocket.emit('keyPress', { inputId: 'right', state: true});
-            else if (event.keyCode == downkey)
-                mySocket.emit('keyPress', { inputId: 'down', state: true});
-            else if (event.keyCode == leftkey)
-                mySocket.emit('keyPress', { inputId: 'left', state: true});
-            else if (event.keyCode == upkey)
-                mySocket.emit('keyPress', { inputId: 'up', state: true});
-            else if (event.keyCode == attackkey)
-                mySocket.emit('keyPress', { inputId: 'shoot', state: true});
-        },
-    control_up:function(event){
+    show:function(){
+      console.log(this.upkey);
+    },
+    getKeyDown:function(e){
+      //if (!inTextField(event)) //채팅창에 포커싱이 되어있을때, 방향키 입력이 안먹게 하는 코드
+          if (e.keyCode === this.rightkey)
+              this.mySocket.emit('keyPress', { inputId: 'right', state: true});
+          else if (e.keyCode === this.downkey)
+              this.mySocket.emit('keyPress', { inputId: 'down', state: true});
+          else if (e.keyCode === this.leftkey)
+            this.mySocket.emit('keyPress', { inputId: 'left', state: true});
+          else if (e.keyCode === this.upkey)
+            this.mySocket.emit('keyPress', { inputId: 'up', state: true});
+          else if (e.keyCode === this.attackkey)
+            this.mySocket.emit('keyPress', { inputId: 'shoot', state: true});
+      },
+    getKeyUp:function(e){
+      
     //if (!inTextField(event)) {//채팅창에 포커싱이 되어있을때, 방향키 입력이 안먹게 하는 코드
-            if (event.keyCode == rightkey)
-                mySocket.emit('keyPress', { inputId: 'right', state: false });
-            else if (event.keyCode == downkey) 
-                mySocket.emit('keyPress', { inputId: 'down', state: false });
-            else if (event.keyCode == leftkey)
-                mySocket.emit('keyPress', { inputId: 'left', state: false });
-            else if (event.keyCode == upkey)
-                mySocket.emit('keyPress', { inputId: 'up', state: false });
-            else if (event.keyCode == attackkey)
-                mySocket.emit('keyPress', { inputId: 'shoot', state: false });
+            if (e.keyCode === this.rightkey)
+              this.mySocket.emit('keyPress', { inputId: 'right', state: false});
+            else if (e.keyCode === this.downkey) 
+              this.mySocket.emit('keyPress', { inputId: 'down', state: false});
+            else if (e.keyCode === this.leftkey)
+              this.mySocket.emit('keyPress', { inputId: 'left', state: false});
+            else if (e.keyCode === this.upkey)
+              this.mySocket.emit('keyPress', { inputId: 'up', state: false});
+            else if (e.keyCode === this.attackkey)
+              this.mySocket.emit('keyPress', { inputId: 'shoot', state: false});
             }
+            
     
 }
