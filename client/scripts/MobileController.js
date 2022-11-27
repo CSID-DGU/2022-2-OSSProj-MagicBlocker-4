5b03e53fc69fd87ff1fb3e47c77e4572b93c0ce2
kh9486 <fbrkdgus1101@gmail.com>
2022-11-28 04:57:48 +0900

    [22.11.28,류강현](refector) 전체 코드 클래스 화 완료

--- client/scripts/MobileController.js
+++ client/scripts/MobileController.js
@@ -1,46 +1,20 @@
 //
 //MobileController.js
 //
-/*
-let Joy1 = new JoyStick('joyDiv', {}, function(stickData) {
-    //console.log(stickData.xPosition);
-    //console.log(stickData.yPosition);
-    //console.log(stickData.cardinalDirection);
-    //console.log(stickData.x);
-    //console.log(stickData.y);
-
-    let stick = stickData.cardinalDirection
-        //console.log('hello');
-
-        if(stick=='E'){
-            move_right();
-        }
-        if(stick=='W'){
-            move_left();
-        }
-        if(stick=='N'){
-            move_up();
-        }
-        if(stick=='S'){
-            move_down();
-        }
-
-        //대각선이동
-        if(stick=='NW'){
-            move_up();
-            move_left();
-        }
-        if(stick=='SW'){
-            move_down();
-            move_left();
-        }
-        if(stick=='SE'){
-            move_down();
-            move_right();
-        }
-        if(stick=='NE'){
-            move_up();
-            move_right();
-        }
-});
-*/
+function MobileController(mySocket){
+    let Joy1 = new JoyStick('joyDiv', {}, function(stickData) {
+        let stick = stickData.cardinalDirection;
+            //정지시 C
+            if(stick=='E'){
+              mySocket.emit('keyPress',{inputId:'joy_right',state:true});
+            }else if(stick=='W'){
+              mySocket.emit('keyPress',{inputId:'joy_left',state:true});
+            }else if(stick=='N'){
+              mySocket.emit('keyPress',{inputId:'joy_up',state:true});
+            }else if(stick=='S'){
+              mySocket.emit('keyPress',{inputId:'joy_down',state:true});
+            }else if(stick=='C'){
+              mySocket.emit('keyPress',{inputId:'joy_stop'});
+            }
+          });
+}
