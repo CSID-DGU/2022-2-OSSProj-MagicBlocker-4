ac91ebf42a5e17fa67fc8cccaae4f185acafd8bb
kh9486 <fbrkdgus1101@gmail.com>
2022-11-28 04:57:48 +0900

    [22.11.28,류강현](refector) 전체 코드 클래스 화 완료

--- client/scripts/Render.js
+++ client/scripts/Render.js
@@ -7,7 +7,8 @@ function Render(canvas_id,client_data){
 
     this.canvas_id = canvas_id;//렌더링객체를 캔버스와 연결해야 사용가능    
     this.my_canvas=document.getElementById(canvas_id);
-    const my_canvas = this.my_canvas; //내부 함수가 사용하기 위한 참조
+    const my_canvas = this.my_canvas;
+    const ctx = my_canvas.getContext("2d"); //내부 함수가 사용하기 위한 참조
     
     //스프라이트 자르기 상수
     const img_frame_index = 100;
@@ -21,59 +22,74 @@ function Render(canvas_id,client_data){
     const bullet_img = new Image();
     bullet_img.src = 'client/sprites/bullet_knight.png';
     //
-    
-
     this.client_data = client_data;//클라이언트 데이터(player와 bullet의 좌표,방향)를 참조하여 렌더링
-    const client_data = this.client_data;
-    
+
+    //게임화면 캔버스 크기를 window크기에 맞춰서 자동변환.
+    function auto_scaile(){
+        my_canvas.width = window.innerWidth;
+        my_canvas.height = window.innerHeight;
+        my_canvas.font = '30px Arial';
+        //게임화면 크기를 조절하면, 이벤트가 발생해서, 이벤트가 발생했을 때만 다시 캔버스 크기를 조정한다(윈도우 크기로)
+        //브라우저 크기를 늘렸다 줄이면 캔버스크기가 맞게 변화한다.(Auto Scaling)
+        window.addEventListener("resize",()=>{
+            my_canvas.width = window.innerWidth;
+            my_canvas.height = window.innerHeight;
+            my_canvas.font = '30px Arial';
+        });
+    }
+
+    //클라이언트 데이터 객체에서 뽑아낸 좌표 데이터로 한 프레임을 화면에 그림. main함수에서 setInterval안에 넣어서 framarate와 함께 사용할 것.
     this.draw_client_data=function(){
+        auto_scaile();
+        
         const player_pack = client_data.get_player_pack();
         const bullet_pack = client_data.get_bullet_pack();
         for(let player of player_pack){
+            ctx.fillText(player.username + ": " + player.points, player.x, player.y); //닉네임 표시
             draw_player(player);
         }
-        /*
-        for(let bullet of bullet_pack){
+        for(let bullet of bullet_pack){    
             draw_bullet(bullet);
         }
-        */
-        console.log('drawing!');
     }
 
     function draw_player(player){
-
-       switch(player.direction){
+        //ctx.drawImage(player_img, 0, 0, img_width, img_height, 500, 200, img_width, img_height); 
+        switch(player.direction){
             case 'down':
-                my_canvas.drawImage(player_img, 0, 0, img_width, img_height, player.x, player.y, img_width, img_height);
+                ctx.drawImage(player_img, 0, 0, img_width, img_height, player.x, player.y, img_width, img_height);
                 break;
             case 'up':
-                my_canvas.drawImage(player_img, img_frame_index, 0, img_width, img_height, player.x, player.y, img_width, img_height);
+                ctx.drawImage(player_img, img_frame_index, 0, img_width, img_height, player.x, player.y, img_width, img_height);
                 break;
             case 'left':
-                my_canvas.drawImage(player_img, img_frame_index * 2, 0, img_width, img_height, player.x, player.y, img_width, img_height);
+                ctx.drawImage(player_img, img_frame_index * 2, 0, img_width, img_height, player.x, player.y, img_width, img_height);
                 break;
             case 'right':
-                my_canvas.drawImage(player_img, img_frame_index * 3, 0, img_width, img_height, player.x, player.y, img_width, img_height);
+                ctx.drawImage(player_img, img_frame_index * 3, 0, img_width, img_height, player.x, player.y, img_width, img_height);
                 break;
        }
+       
+
     }
 
-    function drawBullet(bullet){
+    function draw_bullet(bullet){
 
         switch(bullet.direction){
             case 'down':
-                canvas.drawImage(bullet_img, 0, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
+                ctx.drawImage(bullet_img, 0, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
                 break;
             case 'up':
-                canvas.drawImage(bullet_img, img_frame_index, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
+                ctx.drawImage(bullet_img, img_frame_index, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
                 break;
             case 'left':
-                canvas.drawImage(bullet_img, img_frame_index * 2, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
+                ctx.drawImage(bullet_img, img_frame_index * 2, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
                 break;
             case 'right':
-                canvas.drawImage(bullet_img, img_frame_index * 3, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
+                ctx.drawImage(bullet_img, img_frame_index * 3, 0, img_width, img_height, bullet.x, bullet.y, img_width, img_height);
                 break;
         }
+        
     }
 }
 
