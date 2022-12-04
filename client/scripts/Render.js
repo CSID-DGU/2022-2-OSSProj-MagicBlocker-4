7745a8aa526344523318892c8a30bb001e9b14c8
88sprout <88sprout@gmail.com>
2022-12-05 05:48:03 +0900

    [22.12.05,전재호](feat) 접속자 표시 기능 추가

--- client/scripts/Render.js
+++ client/scripts/Render.js
@@ -64,10 +64,16 @@ function Render(canvas_id,client_data){
     my_canvas.height=1000;
     this.draw_client_data=function(){
         auto_scaile();
+<<<<<<< HEAD
         ctx.clearRect(0, 0, my_canvas.width, my_canvas.height);
+=======
+        document.getElementById(PLAYER_LIST_ID).innerHTML = ''; //접속자 잔상 제거
+
+>>>>>>> ff4ac33 ([22.12.05,전재호](feat) 접속자 표시 기능 추가)
         const player_pack = client_data.get_player_pack();
         const bullet_pack = client_data.get_bullet_pack();
         for(let player of player_pack){
+            document.getElementById(PLAYER_LIST_ID).innerHTML += '<div>' + player.username + '</div>'; //접속자 표시
             ctx.fillText(player.username+"/"+player.hp,player.x,player.y-10); //닉네임 표시
             draw_player(player);
         }
