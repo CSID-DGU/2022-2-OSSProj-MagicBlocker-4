ce4caef23fa728272f9587d79006bf411a7a5d02
kh9486 <fbrkdgus1101@gmail.com>
2022-12-05 01:52:04 +0900

    [22.12.05,류강현](feat) 이름없이 접속했을때 사용자(id)가 뜨도록 수정

--- client/scripts/Ui.js
+++ client/scripts/Ui.js
@@ -6,11 +6,11 @@ function Ui(my_socket,client_data){
     this.my_socket = my_socket;
     this.GAME_CANVAS_ID = "gameCanvas";//렌더링매니저와 연결하기 위한 인터페이스    
     this.JOYSTICK_ID = "joyDiv"; //조이스틱과 연결하기 위한 인터페이스
-    this.selected_charactor = 'none';
+    this.selected_char = 'none';
 
     GAME_CANVAS_ID=this.GAME_CANVAS_ID;//생성자 내부함수는 this에 접근 불가
     JOYSTICK_ID=this.JOYSTICK_ID;
-    selected_charactor = 'none';
+    selected_char = 'none';
 
 
     this.create_login_ui=function(){
@@ -40,6 +40,7 @@ function Ui(my_socket,client_data){
         ui_name_input.classList.add('ui');
         ui_name_input.id='username_input';
         ui_name_input.setAttribute('placeholder','Please Enter Nickname');
+        ui_name_input.setAttribute('maxlength','8');
 
         ui_div.appendChild(ui_name_input);
 
@@ -49,41 +50,52 @@ function Ui(my_socket,client_data){
         ui_play_button.innerHTML='Play';
 
         ui_play_button.onclick = function(){
-            ui_div.style.display = 'none';
-            my_socket.emit('signIn', { username: document.getElementById("username_input").value.trim()});
+            let userid = document.getElementById("username_input").value.trim()
+                    if(userid===""){ //이름을 입력하지않으면 기본이름으로 접속
+                        userid="이름없는유저"+Math.floor(Math.random()*10000);
+                    }
+                    if(selected_char==="none"){//캐릭터를 선택하지않으면 랜덤으로 선택
+                        selected_char=client_data.char_list_pick[Math.floor(Math.random()*client_data.char_list_pick.length)];
+                    }
+                    ui_div.style.display = 'none';
+                    my_socket.emit('signIn', {
+                        username: userid,
+                        char:selected_char,
+                    });
         }
     
         ui_div.appendChild(ui_play_button);
 
-        const ui_how_to_play_button = document.createElement('button');//조작법 안내 버튼
-        ui_how_to_play_button.classList.add('ui');
-        ui_how_to_play_button.classList.add('how-to-play');
-        ui_how_to_play_button.innerHTML='How to Play?';
-        ui_div.appendChild(ui_how_to_play_button);
+        const ui_how_to_play = document.createElement('button');//조작법 안내 버튼
+        ui_how_to_play.classList.add('ui');
+        ui_how_to_play.classList.add('how-to-play');
+        ui_how_to_play.innerHTML='이동:WASD 발사:K';
+        ui_div.appendChild(ui_how_to_play);
 
-        ui_how_to_play_button.onclick = function(){ // 조작법 안내 버튼 클릭시 콜백
-            if(document.getElementById("guideID")==null){
-                ui_div.appendChild(ui_guide_page);
-            }else{
-                ui_guide_page.remove();
-            }            
-        }
 
         //캐릭터 선택창
-        const ui_charactor_select=document.createElement('div');
-        const ui_charactor_select_text=document.createElement('p');
+        const ui_char_select=document.createElement('div');
+        ui_char_select.classList.add('ui');
+        ui_char_select.classList.add('char-select');
 
-        ui_charactor_select_text.innerText='당신의 캐릭터를 선택하세요';
-        ui_charactor_select.classList.add('ui');
-        ui_charactor_select.classList.add('charactor-select');
+        const ui_char_select_prompt=document.createElement('p');
+        ui_char_select_prompt.id="char_select_prompt"
 
+        function update_char_selected(){
+            ui_char_select_prompt.id="char_selected_prompt_picked";
+            ui_char_select_prompt.innerText="캐릭터"
+            ui_char_select_prompt.innerText=client_data.charname_list[selected_char];
+        }
+        
 
-        ui_charactor_select.id='charactor-select';
-        ui_div.appendChild(ui_charactor_select);
-        ui_charactor_select.appendChild(ui_charactor_select_text);
+        ui_char_select_prompt.innerText='캐릭터를 선택하세요';
+    
+        ui_char_select.id='char-select';
+        ui_div.appendChild(ui_char_select);
+        ui_char_select.appendChild(ui_char_select_prompt);
 
         //캐릭터 선택창 세부 캐릭터 선택 버튼 블록
-        for(item of client_data.char_list){
+        for(item of client_data.char_list_pick){
             //console.log(item);
             let temp_char_button = document.createElement('button');
             temp_char_button.id = item+'-id';
@@ -91,23 +103,19 @@ function Ui(my_socket,client_data){
 
             const char_button_image = new Image();
             char_button_image.src = 'client/sprites/sprite_select/'+item+'_select.png';
-            ui_charactor_select.appendChild(temp_char_button);
+            ui_char_select.appendChild(temp_char_button);
             temp_char_button.appendChild(char_button_image);
         }
         
         //onclick 함수는 for문으로 순회가 불가능하다. (왠진모름) 클로저를 사용해야함. (*stackoverflow 참고)
-        for(item of client_data.char_list){
+        for(item of client_data.char_list_pick){
             (function(closure){
                 document.getElementById(item+'-id').onclick=function(){
-                    ui_div.style.display = 'none';
-                    my_socket.emit('signIn', { 
-                        username: document.getElementById("username_input").value.trim(),
-                        char:closure,
-                    });
-                };
+                    selected_char=closure;
+                    update_char_selected();
+                }  
             })(item);
         }
-        
 
         //모바일 컨트롤러
         mobile_controller_div=document.createElement('div');
--- server_data/constants/Constants.js
+++ server_data/constants/Constants.js
@@ -11,4 +11,9 @@
  const STARTING_CHAR = 'warrior';
  const MONGO_REPO = "Account";
  const BULLET_SPEED = 20;
- const COOL_TIME = 30;
+
+ const COOL_TIME = 20;
+<<<<<<< HEAD
+>>>>>>> 9200ee8 ([22.12.05,전재호](fix) 체력 추가, 다른 직업의 bullet image가 보이는 오류 fix)
+=======
+>>>>>>> ae06adc ([22.12.05,전재호])(feat) ghost관전기능 추가, hit함수 오류 fix)
